353133f5c509a0a2b85856e865e2fdac804a1eae
Mandy-cyber <acm.rodriques@gmail.com>
2023-08-05 11:56:38 -0400

    creating camera

---
+++ game_code/camera.py
@@ -0,0 +1,23 @@
+import pygame
+from game_code.constants import *
+
+class Camera(pygame.sprite.Group):
+
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.offset = pygame.math.Vector2()
+
+
+	def custom_draw(self, player):
+		# shifting to make it look like a camera
+		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
+		# draw layers in correct order
+		for layer_idx in LAYERS.values():
+			for sprite in self.sprites():
+				if sprite._layer == layer_idx:
+					offset_rect = sprite.rect.copy()
+					offset_rect.center -= self.offset
+					self.display_surface.blit(sprite.image, offset_rect)
