ebfa7916abcc021bc29e9688ace02c8caf00f43d
Mandy-cyber <acm.rodriques@gmail.com>
2023-08-05 14:08:46 -0400

    added Interaction sprite

--- game_code/sprites/basic_sprite.py
+++ game_code/sprites/basic_sprite.py
@@ -1,6 +1,7 @@
 import pygame
 from game_code.constants import *
 
+
 class BasicSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surface, groups, layer = LAYERS['player']):
         super().__init__(groups)
@@ -9,3 +10,11 @@ class BasicSprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.2)
         self._layer = layer
+
+
+
+class Interaction(BasicSprite):
+    def __init__(self, pos, size, groups, name):
+        surf = pygame.Surface(size) # not concerned with actually seeing this surface
+        super().__init__(pos, surf, groups)
+        self.name = name
