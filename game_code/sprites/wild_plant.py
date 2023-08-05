dfe0f12b7b92364a7f14496fbc783c22b710b364
Mandy-cyber <acm.rodriques@gmail.com>
2023-08-05 13:16:45 -0400

    created WildPlant sprite

---
+++ game_code/sprites/wild_plant.py
@@ -0,0 +1,7 @@
+from game_code.sprites.basic_sprite import BasicSprite
+from ..constants import *
+
+class WildPlant(BasicSprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(pos, self.frames[self.frame_index], groups, LAYERS['player'])
+        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
