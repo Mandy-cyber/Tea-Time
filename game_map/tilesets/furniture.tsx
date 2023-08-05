160fcc82cfddff0fe3d9905a84413d5a0dfcd342
Mandy-cyber <acm.rodriques@gmail.com>
2023-08-05 10:20:59 -0400

    creating first draft of map and tilesets

---
+++ game_map/tilesets/furniture.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="furniture" tilewidth="1024" tileheight="1024" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="512" source="../../assets/images/home/bedroom_rug.PNG"/>
+ </tile>
+ <tile id="1">
+  <image width="1024" height="1024" source="../../assets/images/home/house.PNG"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="../../assets/images/home/house-floor.PNG"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="512" source="../../assets/images/home/nightstand.PNG"/>
+ </tile>
+ <tile id="4">
+  <image width="512" height="512" source="../../assets/images/home/welcome_mat.PNG"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="1024" source="../../assets/images/home/bed.PNG"/>
+ </tile>
+</tileset>
